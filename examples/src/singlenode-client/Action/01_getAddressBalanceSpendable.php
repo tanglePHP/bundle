f292dbf46ade2b3b7c204e19c525067f2ee44d17
SourCL <info@sourcl.com>
2022-09-16 09:52:05 +0200

    add example getAddressBalance

---
+++ examples/src/singlenode-client/Action/01_getAddressBalanceSpendable.php
@@ -0,0 +1,30 @@
+<?php
+  // include tanglePHP autoload from tanglePHP/bundle
+  require_once("../../../../autoload.php");
+
+  use tanglePHP\Network\Connect;
+  use tanglePHP\SingleNodeClient\Action\getBalance;
+
+  // create network connection
+  $network = new Connect('shimmer:testnet');
+
+
+  // get balance with addressbech32
+  echo $ret = (new getBalance($network->singleNode))->address('d4686d71647240aa5d3bdcd54007319e987f29d991c4f970b8b2210e8086162c')
+                                                    ->filter([
+                                                      'hasStorageDepositReturn' => false,
+                                                      'hasExpiration'           => false,
+                                                      'hasTimelock'             => false,
+                                                    ])
+                                                    ->run();
+  // print single information
+  echo PHP_EOL;
+  echo $ret->balance . PHP_EOL;
+  echo $ret->addressBech32 . PHP_EOL;
+  echo $ret->nativeTokens . PHP_EOL;
+  echo $ret->ledgerIndex . PHP_EOL;
+  // print filter informations
+  echo $ret->filter . PHP_EOL;
+  // print network informations
+  echo $ret->networkInfo;
+
---
+++ examples/src/singlenode-client/Action/01_getAddressBalanceTotal.php
@@ -0,0 +1,24 @@
+<?php
+  // include tanglePHP autoload from tanglePHP/bundle
+  require_once("../../../../autoload.php");
+
+  use tanglePHP\Network\Connect;
+  use tanglePHP\SingleNodeClient\Action\getBalance;
+
+  // create network connection
+  $network = new Connect('shimmer:testnet');
+
+
+  // get balance with addressbech32
+  echo $ret = (new getBalance($network->singleNode))->address('rms1qpqjkprc4c8ft2nrtaz7xktgsn3xwzcvn9q2mn7d59ykgmmycf7fwnn25nz')
+                                                    ->run();
+  // print single information
+  echo PHP_EOL;
+  echo $ret->balance . PHP_EOL;
+  echo $ret->addressBech32 . PHP_EOL;
+  echo $ret->nativeTokens . PHP_EOL;
+  echo $ret->ledgerIndex . PHP_EOL;
+  // print filter informations
+  echo $ret->filter . PHP_EOL;
+  // print network informations
+  echo $ret->networkInfo;
