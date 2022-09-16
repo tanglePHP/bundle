dc624e899bba28beeb4397cadf476926a1171ffb
SourCL <info@sourcl.com>
2022-09-16 09:50:19 +0200

    add example 05_getTreasury

---
+++ examples/src/singlenode-client/Simple/05_getTreasury.php
@@ -0,0 +1,9 @@
+<?php
+  // include tanglePHP autoload from tanglePHP/bundle
+  require_once("../../../../autoload.php");
+
+  // create network connection
+  $network = new \tanglePHP\Network\Connect('shimmer:testnet');
+
+  // print result
+  echo $ret = $network->singleNode->treasury();
