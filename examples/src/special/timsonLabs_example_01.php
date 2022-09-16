f2acf53601561040b70e9fc9fc50b92ac606d534
SourCL <info@sourcl.com>
2022-09-16 12:49:56 +0200

    update example timsonLabs_example_01

--- examples/src/special/timsonLabs_example_01.php
+++ examples/src/special/timsonLabs_example_01.php
@@ -2,15 +2,20 @@
   // include tanglePHP autoload from tanglePHP/bundle
   require_once("../../../autoload.php");
 
+  use tanglePHP\Core\Exception\Api;
+  use tanglePHP\Core\Exception\Converter;
+  use tanglePHP\Core\Exception\Helper;
+  use tanglePHP\Core\Helper\JSON;
   use tanglePHP\Network\Connect;
   use tanglePHP\SingleNodeClient\Action\getBalance;
+  use tanglePHP\SingleNodeClient\Models\ReturnAddressBalance;
 
   /**
    * Class timsonLabs_example_01
    *
    * @author       Stefan Braun <stefan.braun@tanglePHP.com>
    * @copyright    Copyright (c) 2022, StefanBraun
-   * @version      2022.09.16-1223
+   * @version      2022.09.16-1249
    */
   class timsonLabs_example_01 {
     /**
@@ -30,12 +35,17 @@
     }
 
     /**
-     * @return \tanglePHP\Core\Helper\JSON|\tanglePHP\SingleNodeClient\Models\ReturnAddressBalance
-     * @throws \tanglePHP\Core\Exception\Api
-     * @throws \tanglePHP\Core\Exception\Converter
-     * @throws \tanglePHP\Core\Exception\Helper
+     * @param string $address
+     * @param bool   $calcMarketData
+     *
+     * @return JSON|ReturnAddressBalance
+     * @throws Api
+     * @throws Converter
+     * @throws Helper
      */
-    public function getBalance(string $address, bool $calcMarketData = true): \tanglePHP\SingleNodeClient\Models\ReturnAddressBalance {
+    public function getBalance(string $address, bool $calcMarketData = true): JSON|ReturnAddressBalance {
+
+      $useNetwork = null;
 
       if(str_starts_with($address, 'smr')) {
         $useNetwork = $this->networks['shimmer'] = $this->networks['shimmer'] ?? new Connect('shimmer:mainnet');
@@ -49,35 +59,41 @@
       elseif(str_starts_with($address, 'atoi')) {
         $useNetwork = $this->networks['iota_devnet'] = $this->networks['iota_devnet'] ?? new Connect('iota:devnet');
       }
+
       $ret = (new getBalance($useNetwork))->address($address)
                                           ->run();
-      if($calcMarketData) {
-        $ret->calcedMarketData = $this->calcMarketData((int)$ret->balance, $ret->marketData->__toArray());
+      if(isset($ret->balance)) {
+        if($calcMarketData) {
+          $ret->calcedMarketData = $this->calcMarketData((int)$ret->balance, $ret->marketData->__toArray());
+        }
       }
 
       return $ret;
     }
 
+    /**
+     * @param int   $balance
+     * @param array $marketData
+     *
+     * @return array
+     */
     public function calcMarketData(int $balance, array $marketData): array {
       [$coin] = array_keys($marketData);
       //
-      $ret = [
-        'balance' => $balance,
+      return [
+        'balance'     => $balance,
         'balanceCalc' => $balance / 1000000,
-        'coin'    => $coin,
-        'price'   => $marketData[$coin],
-        'calc'    => [
+        'coin'        => $coin,
+        'price'       => $marketData[$coin],
+        'calc'        => [
           'usd' => $balance / 1000000 * $marketData[$coin]['usd'],
           'eur' => $balance / 1000000 * $marketData[$coin]['eur'],
         ],
       ];
-
-      return $ret;
     }
   }
   $example = new timsonLabs_example_01();
   $ret     = $example->getBalance('atoi1qqghchyvvegq3jt0a4jg5392t63zmfhk0pkqj8czpelr7ey67ekgsat9sky', true);
-
   print_r($ret->calcedMarketData);
 
   // outputs like this
