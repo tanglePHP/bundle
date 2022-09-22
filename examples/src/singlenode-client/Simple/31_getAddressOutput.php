049d5e292655936d4d57a2402f89166c10d7bb6c
SourCL <info@sourcl.com>
2022-09-22 12:15:47 +0200

    update example

--- examples/src/singlenode-client/Simple/31_getAddressOutput.php
+++ examples/src/singlenode-client/Simple/31_getAddressOutput.php
@@ -3,7 +3,7 @@
   require_once("../../../../autoload.php");
 
   // create network connection
-  $network = new \tanglePHP\Network\Connect('shimmer:testnet');
+  $network = new \tanglePHP\Network\Connect('iota:devnet');
 
   // print result
-  echo $ret = $network->singleNode->Plugin->Indexer->outputsBasicAddress('rms1qr2xsmt3v3eyp2ja80wd2sq8xx0fslefmxguf7tshzezzr5qsctzc2f5dg6');
+  echo $ret = $network->singleNode->addressOutput('atoi1qrnhxtx78lg577jrffd5y6scv9lxj8maelmvrq2whsjnplr0llp3ucyd3rm');
