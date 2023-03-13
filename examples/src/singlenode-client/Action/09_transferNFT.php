39f87aa35354f6723da7b7c9e9a3dc2d7fa2e538
SourCL <info@sourcl.com>
2023-03-13 08:08:10 +0100

    update example

--- examples/src/singlenode-client/Action/09_transferNFT.php
+++ examples/src/singlenode-client/Action/09_transferNFT.php
@@ -11,6 +11,8 @@
   $mnemonic = require_once("../../../.assets/createMnemonic.php");
   // transfer NFT
   echo $ret = (new transferNFT($network->singleNode))->seedInput($mnemonic)
+                                                     ->accountIndex(0)
+                                                     ->addressIndex(0)
                                                      ->nftId('d63a4a3b7d080a5f9f2bfdd83e62a8b90f287977ff1c1cb10ae34d857630c1a0')
                                                      ->toAddress('rms1qr2xsmt3v3eyp2ja80wd2sq8xx0fslefmxguf7tshzezzr5qsctzc2f5dg6')
                                                      ->run();
